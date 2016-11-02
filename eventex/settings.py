da3c18bf16c44d77407d71acd65664333410e969
TakenakaRoberta <takenaka.roberta@gmail.com>
2016-11-02 11:32:04 -0200

    Inscricao com email

--- eventex/settings.py
+++ eventex/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_POST = config('EMAIL_POST', cast=int)
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
